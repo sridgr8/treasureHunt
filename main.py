4f61e06181a5a83613802735590ca1d1c6e5e4bb
Srinivasulu K <sridgr8.13@gmail.com>
2021-11-13 23:38:43 +0530

    Updated Code v1.0

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver import ActionChains
 import time
 from time import sleep
 
@@ -26,10 +29,42 @@ def pageThree():
         except:
             print("Correct btn was clicked.")
 
-def pageFour():#in progress
+def pageFour():
     time.sleep(2)
-    driver.execute_script('document.getElementsByTagName("video")[0].play()')
-    time.sleep(2)
+    driver.switch_to.frame(driver.find_element_by_xpath('//iframe[starts-with(@src, "https://www.youtube.com/embed")]'))
+    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//button[@aria-label="Play"]'))).click()
+    time.sleep(9)
+    print("Trying the move")
+    driver.switch_to.default_content()
+    driver.find_element(By.XPATH, "/html/body/div[2]/div/div/div/div/div/div[2]/div").click()
+    # time.sleep(1)
+    driver.find_element(By.XPATH, "/html/body/div[2]/div/div/div/div/div/div[2]/div").click()
+    driver.switch_to.frame(driver.find_element_by_xpath('//iframe[starts-with(@src, "https://www.youtube.com/embed")]'))
+    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//button[@aria-label="Mute (m)"]'))).click()
+    time.sleep(3)
+    driver.switch_to.default_content()
+    driver.find_element(By.XPATH, "//*[@id='aVideoSubmit']").click()
+    
+def pageFive():#in progress
+    time.sleep(20)
+    
+    
+
+def move_Left():
+    time.sleep(1)
+    driver.find_element(By.XPATH, "//div/a/i[text()='arrow_back']").click()
+
+def move_Right():
+    time.sleep(1)
+    driver.find_element(By.XPATH, "//div/a/i[text()='arrow_forward']").click()
+
+def move_Up():
+    time.sleep(1)
+    driver.find_element(By.XPATH, "//div/a/i[text()='arrow_upward']").click()
+
+def move_Down():
+    time.sleep(1)
+    driver.find_element(By.XPATH, "//div/a/i[text()='arrow_downward']").click()
 
 def close_driver():
     time.sleep(2)
@@ -41,6 +76,7 @@ print("Execution Started")
 driver = webdriver.Chrome(executable_path="./webDrivers/chromedriver")
 
 startExecution()
+
 pageOne()
 
 pageTwo()
@@ -48,6 +84,8 @@ pageTwo()
 pageThree()
 
 pageFour()
+
+pageFive()
 
 close_driver()
 print("Execution Completed.")
