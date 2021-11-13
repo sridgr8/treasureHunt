55584539b2b7f0ad1bff523c24a49fb160be8b2e
Srinivasulu K <sridgr8.13@gmail.com>
2021-11-14 02:31:40 +0530

    Code Updated v2.0

--- main.py
+++ main.py
@@ -5,7 +5,12 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver import ActionChains
 import time
+import numpy
 from time import sleep
+
+maze_List1 = [6,2,2,2,6,2,8,1,6,3,2,2,4,1,2,1,4,3,2,1,4,1,2,2,6,6,8,2,6,1,8,1,6,2]
+maze_List2 = [6,2,2,1,6,2,8,3,4,2,8,2,6,1,8,2,6,2,2,3,6,2,2,4,6,1,2,1,6,3]
+maze_List = []
 
 def startExecution():
     time.sleep(2)
@@ -27,14 +32,13 @@ def pageThree():
         try:
             btn.click()
         except:
-            print("Correct btn was clicked.")
+            pass
 
 def pageFour():
     time.sleep(2)
     driver.switch_to.frame(driver.find_element_by_xpath('//iframe[starts-with(@src, "https://www.youtube.com/embed")]'))
     WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//button[@aria-label="Play"]'))).click()
     time.sleep(9)
-    print("Trying the move")
     driver.switch_to.default_content()
     driver.find_element(By.XPATH, "/html/body/div[2]/div/div/div/div/div/div[2]/div").click()
     # time.sleep(1)
@@ -46,9 +50,36 @@ def pageFour():
     driver.find_element(By.XPATH, "//*[@id='aVideoSubmit']").click()
     
 def pageFive():#in progress
-    time.sleep(20)
-    
-    
+    time.sleep(1)
+    get_Matrix()
+
+def make_List(l1):
+    l1_copy =[]
+    for item in l1: l1_copy.append(item)
+    return l1_copy
+
+def get_Matrix():
+    time.sleep(1)
+    if("green" in str((driver.find_element(By.XPATH, "//*[@id='maze']/tr[7]/td[12]").get_attribute("class")))):
+        maze_List=make_List(maze_List1)
+    else:
+        maze_List=make_List(maze_List2)
+
+    i = 0
+    while i < (len(maze_List) - 1):
+        # print(maze_List2[i])
+        if(maze_List[i]==2):
+            for _ in range(maze_List[i+1]):move_Down()
+        elif(maze_List[i]==4):
+            for _ in range(maze_List[i+1]):move_Left()
+        elif(maze_List[i]==6):
+            for _ in range(maze_List[i+1]):move_Right()
+        elif(maze_List[i]==8):
+            for _ in range(maze_List[i+1]):move_Up()
+        i += 2
+    driver.find_element(By.XPATH, "//*[@id='crystalMazeFormSubmit']").click()
+    time.sleep(5)
+
 
 def move_Left():
     time.sleep(1)
